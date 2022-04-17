33549becdce97bd223d360bc64bfa249f632a48f
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-04-18 00:37:58 +0100

    added view to componentScreen

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-  return <Text style={styles.textStyle}>I am ComponentsScreen</Text>;
+  let greeting = "Hi There!";
+  return (
+    <View>
+      <Text style={styles.textStyle}>This is ComponentsScreen</Text>
+      <Text>{greeting}</Text>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
